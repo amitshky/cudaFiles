34d698eeade81afd524e64a8c8f3c60528ffe592
amitshky <a7shky@gmail.com>
2022-02-03 14:10:36 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# cudaFiles
+Repo for CUDA files.
+This repo has only been built and tested in Windows.
+
+## Prerequisites
+* [cmake](https://cmake.org/download/)
+* [CUDA runtime library](https://developer.nvidia.com/cuda-downloads) (This repo was built and tested on v11.6)
+
+## Configuration
+Configure cmake build files.\
+(Run these commands in the root directory of this repo.)
+### For Windows
+```
+./config.bat
+```
+
+## Build and run
+(Run these commands in the root directory of this repo.)
+### For Windows
+Build and run in Debug mode.
+```
+./build.bat
+./run.bat
+```
+
+Build and run in Release mode.
+```
+./build-rel.bat
+./run-rel.bat
+```
+
+## Output
+Some screenshots of the outputs of the programs.
+
+<!--* write image caption
+
+<img src="img/image.png" width=192>-->
+
+* Grayscale
+
+<img src="img/grayscale.png" width=192>
+
+## Resources
+* [CUDA C++ Programming Guide](https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html)
+* [NVCC commands manual](https://helpmanual.io/help/nvcc/)
+* [CUDA - Modern CMake](https://cliutils.gitlab.io/modern-cmake/chapters/packages/CUDA.html)
+* [Building Cross-Platform CUDA Applications with CMake](https://developer.nvidia.com/blog/building-cuda-applications-cmake/)
+* [CUDA by Example](https://developer.nvidia.com/cuda-example)
+* [CUDA Memory model](https://www.3dgep.com/cuda-memory-model/)
---
+++ src/grayscale.cu
@@ -0,0 +1,81 @@
+#include <cstdio>
+#include <cstdint>
+#include <cuda_runtime.h>
+
+#include "stb_image/stb_image.h"
+#include "stb_image/stb_image_write.h"
+
+
+__global__ void Grayscale(uint8_t* imgData, const int width, const int height, const int channels)
+{
+	// mapping thread id as 2D array indexes
+	int x = threadIdx.x + blockDim.x * blockIdx.x;
+	int y = threadIdx.y + blockDim.y * blockIdx.y;
+
+	// check image boundary
+	if (x >= width || y >= height)
+		return;
+	
+	// calc index of image data (1 Dim) (with offset for channels)
+	int i = (x + y * width) * channels;
+
+	// grayscale calc
+	uint8_t r = 0.2126f * imgData[i];
+	uint8_t g = 0.7152f * imgData[i + 1];
+	uint8_t b = 0.0722f * imgData[i + 2];
+
+	// write data to image buffer
+	imgData[i]     = r + g + b; // red channel
+	imgData[i + 1] = r + g + b; // green channel
+	imgData[i + 2] = r + g + b; // blue channel
+}
+
+int main()
+{
+	// kernel properties
+	constexpr int tx = 32;
+	constexpr int ty = 32;
+	// image properties
+	const char* inPath  = "img/input.png";
+	const char* outPath = "img/grayscale.png";
+	int width    = 0;
+	int height   = 0;
+	int channels = 0;
+
+	// load image
+	stbi_set_flip_vertically_on_load(true);
+	uint8_t* imgData = stbi_load(inPath, &width, &height, &channels, 0); // input image data
+	uint64_t imgSize = width * height * channels;
+	printf("Input image info:\n\twidth = %d, height = %d, channels = %d\n", 
+		width, height, channels);
+
+	// allocate device memory
+	uint8_t* d_ImgData = nullptr;
+	cudaMalloc(&d_ImgData, imgSize * sizeof(uint8_t));
+	// copy image data from host memory to device memory
+	cudaMemcpy(d_ImgData, imgData, imgSize * sizeof(uint8_t), cudaMemcpyHostToDevice);
+
+	// assign grid size and block size
+	const dim3 threads(tx, ty); // block size
+	const dim3 blocks((width + tx - 1) / tx, (height + ty - 1)/ ty); // grid size
+	printf("Kernel info:\n\tthreads = (%d, %d), blocks = (%d, %d)\n", 
+		threads.x, threads.y, blocks.x, blocks.y);
+
+	// call cuda kernel
+	printf("Converting image to grayscale...\n");
+	Grayscale<<<blocks, threads>>>(d_ImgData, width, height, channels);
+	// copy image data from device memory to host memory
+	cudaMemcpy(imgData, d_ImgData, imgSize * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+
+	// write image
+	stbi_flip_vertically_on_write(true);
+	stbi_write_png(outPath, width, height, channels, imgData, width * channels);
+	printf("Image exported to %s.\n", outPath);
+
+	// free memory
+	cudaFree(d_ImgData);
+	stbi_image_free(imgData);
+	// assign null
+	d_ImgData = nullptr;
+	imgData   = nullptr;
+}
